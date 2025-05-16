c8ea779e38eaf6c4ceb3cc625df5d53b98c4b4d0
qppon2024 <osamu_yumimoto@hotmail.com>
2025-05-16 11:15:31 +0900

    SerialPortHundler.csを追加してsirialpot関係を１つにまとめた。

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -2,88 +2,21 @@
 using System.Text;
 namespace ConsoleApp12
 {
-    internal class Program
+    public class Program
     {
-        static SerialPort serialPort;
-        static PlcProtocolHandler plcProtocolHandler ;
-        static StringBuilder dataBuffer = new StringBuilder();
+  
         static void Main(string[] args)
         {
-            plcProtocolHandler = new ();
-            // シリアルポートの設定
-            const string CR = "\r"; //CR
-            serialPort = new SerialPort("COM3", 38400, Parity.Even, 7, StopBits.Two);
-            serialPort.NewLine = CR;
-            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler); // イベントハンドラを登録
-            serialPort.Open();
+ 
             //RecievePlcResponse();
-            sendData();
+            SerialPortHundler serialPortHundler = new();
+            serialPortHundler.SendData();
             Console.WriteLine("シリアルポートを開きました。データ受信を待っています...");
             Console.WriteLine("終了するにはEnterキーを押してください。");
             Console.ReadLine();
 
-            serialPort.Close(); Console.WriteLine("Hello, World!");
+            serialPortHundler.Close(); Console.WriteLine("Hello, World!");
         }
-        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
-        {
-            try
-            {
-                // データを受信
-                int ret=RecievePlcResponse();
-                Console.WriteLine($"受信データ: {ret}");
-               // Thread.Sleep(1000); // 1秒待機
-                sendData();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"エラー: {ex.Message}");
-            }
-        }
-        private static void sendData()
-        {
-            string message = plcProtocolHandler.BuildReciveCommand();
-            serialPort.WriteLine(message);
-        }
-        private static int RecievePlcResponse()
-        {
-
-            while (true)
-            {
-                if (serialPort.BytesToRead > 0)
-                {
-                    AppendRacieveData();
-
-                    // 改行を検知
-                    if (HasReceivedCompleteResponse(out int bitRead)) return bitRead;
-
-                }
-            }
-        }
-        // HasReceivedCompleteResponseメソッドは、受信したデータが完全な応答であるかどうかを確認します。
-        private static bool HasReceivedCompleteResponse(out int bitRead)
-        {
-            if (dataBuffer.ToString().Contains(serialPort.NewLine))
-            {
-                string completeData = dataBuffer.ToString();
-                Console.WriteLine($"受信データ: {completeData}");
-                dataBuffer.Clear(); // バッファをクリア
-                return plcProtocolHandler.ReadBit(completeData, out bitRead);
-
-
-            }
-            bitRead = 0;
-            return false;
-        }
-        // AppendRacieveDataメソッドは、受信したデータをバッファに追加します。
-        private static void AppendRacieveData()
-        {
-            string received = serialPort.ReadExisting();
-            dataBuffer.Append(received);
-        }
-        // PLCからの応答を受信した後、ビットを取得します。
-
-
-
 
 
     }
---
+++ ConsoleApp12/SerialPortHundler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class SerialPortHundler
+
+    {
+        SerialPort serialPort;
+        PlcProtocolHandler plcProtocolHandler;
+        StringBuilder dataBuffer = new StringBuilder(1200);
+        public SerialPortHundler()
+        {
+            plcProtocolHandler = new();
+            // シリアルポートの設定
+            const string CR = "\r"; //CR
+            serialPort = new SerialPort("COM3", 38400, Parity.Even, 7, StopBits.Two);
+            serialPort.NewLine = CR;
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler); // イベントハンドラを登録
+            serialPort.Open();
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                // データを受信
+                int ret = RecievePlcResponse();
+                Console.WriteLine($"受信データ: {ret}");
+                // Thread.Sleep(1000); // 1秒待機
+                SendData();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"エラー: {ex.Message}");
+            }
+        }
+        public void SendData()
+        {
+            string message = plcProtocolHandler.BuildReciveCommand();
+            serialPort.WriteLine(message);
+        }
+        public void Close()
+        {
+            serialPort.Close();
+        }
+        private int RecievePlcResponse()
+        {
+
+            while (true)
+            {
+                if (serialPort.BytesToRead > 0)
+                {
+                    AppendRacieveData();
+
+                    // 改行を検知
+                    if (HasReceivedCompleteResponse(out int bitRead)) return bitRead;
+
+                }
+            }
+        }
+        // HasReceivedCompleteResponseメソッドは、受信したデータが完全な応答であるかどうかを確認します。
+        private bool HasReceivedCompleteResponse(out int bitRead)
+        {
+            if (dataBuffer.ToString().Contains(serialPort.NewLine))
+            {
+                string completeData = dataBuffer.ToString();
+                Console.WriteLine($"受信データ: {completeData}");
+                dataBuffer.Clear(); // バッファをクリア
+                return plcProtocolHandler.ReadBit(completeData, out bitRead);
+
+
+            }
+            bitRead = 0;
+            return false;
+        }
+        // AppendRacieveDataメソッドは、受信したデータをバッファに追加します。
+        private void AppendRacieveData()
+        {
+            string received = serialPort.ReadExisting();
+            dataBuffer.Append(received);
+        }
+        // PLCからの応答を受信した後、ビットを取得します。
+
+
+    }
+}
